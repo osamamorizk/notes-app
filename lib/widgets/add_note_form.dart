1751def006e1d59ae2202b4da60acfd0ab01be82
osamamorizk <osamarizk262@gmail.com>
2024-08-10 20:36:41 +0300

    add note loading

--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -50,21 +50,26 @@ class _AddNoteFormState extends State<AddNoteForm> {
           const SizedBox(
             height: 32,
           ),
-          CustomButton(
-            onTap: () {
-              if (globalKey.currentState!.validate()) {
-                globalKey.currentState!.save();
-                var note = NoteModel(
-                  date: DateTime.now().toString(),
-                  title: title!,
-                  content: subtitle!,
-                  color: Colors.yellow.value,
-                );
-                BlocProvider.of<AddNoteCubit>(context).addNote(note);
-              } else {
-                autovalidateMode = AutovalidateMode.always;
-                setState(() {});
-              }
+          BlocBuilder<AddNoteCubit, AddNoteState>(
+            builder: (context, state) {
+              return CustomButton(
+                isLoading: state is AddNoteLoading ? true : false,
+                onTap: () {
+                  if (globalKey.currentState!.validate()) {
+                    globalKey.currentState!.save();
+                    var note = NoteModel(
+                      date: DateTime.now().toString(),
+                      title: title!,
+                      content: subtitle!,
+                      color: Colors.yellow.value,
+                    );
+                    BlocProvider.of<AddNoteCubit>(context).addNote(note);
+                  } else {
+                    autovalidateMode = AutovalidateMode.always;
+                    setState(() {});
+                  }
+                },
+              );
             },
           ),
           const SizedBox(
--- lib/widgets/add_note_to_bottom_sheet.dart
+++ lib/widgets/add_note_to_bottom_sheet.dart
@@ -27,9 +27,9 @@ class AddNoteBottomSheet extends StatelessWidget {
             }
           },
           builder: (context, state) {
-            return ModalProgressHUD(
-                inAsyncCall: state is AddNoteLoading ? true : false,
-                child: SingleChildScrollView(child: AddNoteForm()));
+            return AbsorbPointer(
+                absorbing: state is AddNoteLoading ? true : false,
+                child: const SingleChildScrollView(child: AddNoteForm()));
           },
         ),
       ),
